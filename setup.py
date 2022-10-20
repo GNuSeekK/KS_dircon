d648b8a6f20db8742740ac6b01a7bb4dd370a76e
KeesungLee <ks.lee@alcherainc.com>
2022-10-20 14:15:44 +0900

    v0.0.1 last

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup
 install_requires = [
     'Keesung_logging @ git+https://github.com/GNuSeekK/Keesung_logging@v0.0.8#egg=Keesung_logging',
     ]
-setup_requires=[
-    'Kessung_logging>=0.0.8'
-    ]
 
 setup(
     name = 'KS_dircon',
@@ -16,6 +13,5 @@ setup(
     author_email='dlrltjdwkd@naver.com',
     license='keesung',
     packages=['KS_dircon'],
-    install_requires=install_requires,
-    setup_requires=setup_requires
+    install_requires=install_requires
 )
